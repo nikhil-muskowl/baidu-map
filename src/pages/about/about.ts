e7feb4200816e8052c2435d82a142488e558a5d5
nikhil-muskowl <nikhil.muskowl@gmail.com>
2018-08-30 10:45:05 +0530

    cxv

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ControlAnchor, MapOptions, NavigationControlOptions, NavigationControlType, Point, MapTypeEnum, MarkerOptions } from 'angular2-baidu-map';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  options: MapOptions;
+  point: Point;
+  navOptions: NavigationControlOptions;
+  public markers: Array<{ point: Point; options?: MarkerOptions }>;
 
+  
   constructor(public navCtrl: NavController) {
 
   }
